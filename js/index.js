1c93cd031f82158244c9280c429b87c74bbe1142
whitehox <oketegah@gmail.com>
2019-02-28 13:27:23 +0100

    Fixed issues with users listings

--- js/index.js
+++ js/index.js
@@ -32,11 +32,12 @@ $(document).ready(function () {
     $('#searchBar').on('keyup', function () {
         $('.searchBar').animate({ top: "0" }, 500).animate({ left: "0" }, 500).animate({ width: "100%" }, 500);
         $('.resultArea').fadeIn();
+
         clearTimeout(timer);
         timer = setTimeout(search, 2000);
     });
     $('#searchBar').on('keydown', function () {
-        // $('.searchBar').animate({top: "50%"}, 500);
+        // $('.searchBar').animate({top: "50%"}, 500)
         clearTimeout(timer);
     });
 
@@ -47,86 +48,86 @@ $(document).ready(function () {
             method: "GET",
             dataType: "json",
             url: `http://localhost:3000/users?category=${text}`,
-            success: function(res) {
-                for(i=0; i <= res.length; i++) {
-                    if(res[i].category == text) {
-                        alert(res[i].username);
+            success: function (res) {
+                $.each(res.category, function (i, v) {
+                    if (v.category == text) {
+                        alert(v.username);
+                        return;
                     }
-                }
+                });
             }
         })
-
     }
-    $("#searchBar").on("keyup", function() {
-        return search();
+$("#searchBar").on("keyup", function () {
+    
+});
+
+$('#submitSignForm').submit(function (e) {
+    e.preventDefault();
+
+    var firstname = $('input[name="firstname"]').val();
+    var lastname = $('input[name="lastname"]').val();
+    var email = $('input[name="email"]').val();
+    var username = $('input[name="username"]').val();
+    var password = $('input[name="password"]').val();
+    var category = $('#category').val();
+    var briefdes = $("#briefdes").val();
+    var fullDate = new Date();
+    var twoDigitMonth = ((fullDate.getMonth().length + 1) === 1) ? (fullDate.getMonth() + 1) : '0' + (fullDate.getMonth() + 1);
+    var currentDate = fullDate.getDate() + "/" + twoDigitMonth + "/" + fullDate.getFullYear();
+
+    $.ajax({
+        method: "POST",
+        url: "http://localhost:3000/users",
+        data: {
+            "firstname": firstname,
+            "lastname": lastname,
+            "email": email,
+            "username": username,
+            "password": password,
+            "category": category,
+            "description": briefdes,
+            "date": currentDate
+        },
+        success: function (res) {
+            alert('Submitted');
+        },
+        beforeSend: function () {
+            $('.logoForm').fadeOut().fadeIn();
+        }
     });
+});
 
-    $('#submitSignForm').submit(function (e) {
-        e.preventDefault();
+$("#submitLogForm").submit(function (e) {
+    e.preventDefault();
+    var username = $("#logusername").val();
+    var password = $("#logpassword").val();
 
-        var firstname = $('input[name="firstname"]').val();
-        var lastname = $('input[name="lastname"]').val();
-        var email = $('input[name="email"]').val();
-        var username = $('input[name="username"]').val();
-        var password = $('input[name="password"]').val();
-        var category = $('#category').val();
-        var briefdes = $("#briefdes").val();
-        var fullDate = new Date();
-        var twoDigitMonth = ((fullDate.getMonth().length + 1) === 1) ? (fullDate.getMonth() + 1) : '0' + (fullDate.getMonth() + 1);
-        var currentDate = fullDate.getDate() + "/" + twoDigitMonth + "/" + fullDate.getFullYear();
-
-        $.ajax({
-            method: "POST",
-            url: "http://localhost:3000/users",
-            data: {
-                "firstname": firstname,
-                "lastname": lastname,
-                "email": email,
-                "username": username,
-                "password": password,
-                "category": category,
-                "description": briefdes,
-                "date": currentDate
-            },
-            success: function (res) {
-                alert('Submitted');
-            },
-            beforeSend: function () {
-                $('.logoForm').fadeOut().fadeIn();
+    $.ajax({
+        type: "GET",
+        dataType: "json",
+        url: `http://localhost:3000/users?username=${username}&password=${password}`,
+        data: { "username": username, "password": password },
+        success: function (res) {
+            if (res.length == 0) {
+                $(".result").append('<p class="resultDanger">Incorrect username or password</p>')
             }
-        });
-    });
 
-    $("#submitLogForm").submit(function (e) {
-        e.preventDefault();
-        var username = $("#logusername").val();
-        var password = $("#logpassword").val();
-
-        $.ajax({
-            type: "GET",
-            dataType: "json",
-            url: `http://localhost:3000/users?username=${username}&password=${password}`,
-            data: { "username": username, "password": password },
-            success: function (res) {
-                if (res.length == 0) {
-                    $(".result").append('<p class="resultDanger">Incorrect username or password</p>')
+            for (i in res) {
+                if (res[i].status == 1) {
+                    $(".result").append('<p class="resultDanger">This account has been deleted.</p>')
                 }
 
-                for (i in res) {
-                    if (res[i].status == 1) {
-                        $(".result").append('<p class="resultDanger">This account has been deleted.</p>')
-                    }
-
-                    else {
-                        window.location.assign(`dashboard.html?username=${username}`);
-                    }
+                else {
+                    window.location.assign(`dashboard.html?username=${username}`);
                 }
-            },
-            beforeSend: function () {
-                setInterval(function () {
-                    $(".logoFrame").fadeOut().fadeIn();
-                }, 1000);
             }
-        });
+        },
+        beforeSend: function () {
+            setInterval(function () {
+                $(".logoFrame").fadeOut().fadeIn();
+            }, 1000);
+        }
     });
+});
 });
