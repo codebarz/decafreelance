005c97ed0592444d9b66a7fe673451fa2f458fcb
whitehox <oketegah@gmail.com>
2019-02-28 15:42:40 +0100

    Fixed user profile issue

--- js/getusers.js
+++ js/getusers.js
@@ -10,34 +10,29 @@ $(document).ready(function () {
         type: "GET",
         url: `http://localhost:3000/users?username!=${param}`,
         success: function (res) {
+            $.each(res, function (index, value) {
+                
+                if(value.username == param) {
 
-            if (res[i].username == param) {
-                allusers = "";
-
-                allusers += `<div class="card" style="display: none>`;
-                allusers += `<div class="card-body">`;
-                allusers += `<h5 class="card-title">${res[i].username}</h5>`;
-                allusers += `<p class="card-text">${res[i].description}</p>`;
-                allusers += `<p class="card-text"><small class="text-muted">${res[i].category}</small></p>`;
-                allusers += `<p class="card-text"><small class="text-muted">Active since ${res[i].date}</small></p>`;
-                allusers += `<a class="cardLink" href="profile.html?username=${param}&view=${res[i].username}">View user</a>`;
-                allusers += `</div>`;
-                allusers += `</div>`;
-            }
-            else {
+                }
+                else {
+                    
                 allusers = "";
 
                 allusers += `<div class="card">`;
                 allusers += `<div class="card-body">`;
-                allusers += `<h5 class="card-title">${res[i].username}</h5>`;
-                allusers += `<p class="card-text">${res[i].description}</p>`;
-                allusers += `<p class="card-text"><small class="text-muted">${res[i].category}</small></p>`;
-                allusers += `<p class="card-text"><small class="text-muted">Active since ${res[i].date}</small></p>`;
-                allusers += `<a class="cardLink" href="profile.html?username=${param}&view=${res[i].username}">View user</a>`;
+                allusers += `<h5 class="card-title">${value.username}</h5>`;
+                allusers += `<p class="card-text">${value.description}</p>`;
+                allusers += `<p class="card-text"><small class="text-muted">${value.category}</small></p>`;
+                allusers += `<p class="card-text"><small class="text-muted">Active since ${value.date}</small></p>`;
+                allusers += `<a class="cardLink" href="profile.html?username=${param}&view=${value.username}">View user</a>`;
                 allusers += `</div>`;
                 allusers += `</div>`;
-            }
-            $("#allusers").append(allusers);
+                $("#allusers").append(allusers);
+                }
+            });
+
+            
         }
     });
 });
--- js/index.js
+++ js/index.js
@@ -20,114 +20,95 @@ $(document).ready(function () {
     $(".edit").click(function () {
         $(".editProfileArea").show();
         $("#allusers").hide();
-        $("#viewProfileAre").hide();
+        $(".viewProfileArea").hide();
     });
     $(".feed").click(function () {
         $(".editProfileArea").hide();
         $("#allusers").show();
         $(".viewProfileArea").hide();
     });
-
-    var timer;
-    $('#searchBar').on('keyup', function () {
+    $("#searchBar").on("keydown", function () {
         $('.searchBar').animate({ top: "0" }, 500).animate({ left: "0" }, 500).animate({ width: "100%" }, 500);
         $('.resultArea').fadeIn();
+        var query = $("#searchBar").val();
+        var expression = new RegExp(query, "i");
+        $.getJSON("db.json", function(data) {
+            $.each(data, function (key, value) {
+                if (value.category.query(expression) != -1 || value.username.query(expression) != -1) {
+                    $(".resultArea").append('<p>' + value.username + '</p>')
+                    alert(value.username);
+                }
 
-        clearTimeout(timer);
-        timer = setTimeout(search, 2000);
-    });
-    $('#searchBar').on('keydown', function () {
-        // $('.searchBar').animate({top: "50%"}, 500)
-        clearTimeout(timer);
+            });
+        });
     });
 
-    function search() {
-        var text = $("#searchBar").val();
+    $('#submitSignForm').submit(function (e) {
+        e.preventDefault();
+
+        var firstname = $('input[name="firstname"]').val();
+        var lastname = $('input[name="lastname"]').val();
+        var email = $('input[name="email"]').val();
+        var username = $('input[name="username"]').val();
+        var password = $('input[name="password"]').val();
+        var category = $('#category').val();
+        var briefdes = $("#briefdes").val();
+        var fullDate = new Date();
+        var twoDigitMonth = ((fullDate.getMonth().length + 1) === 1) ? (fullDate.getMonth() + 1) : '0' + (fullDate.getMonth() + 1);
+        var currentDate = fullDate.getDate() + "/" + twoDigitMonth + "/" + fullDate.getFullYear();
 
         $.ajax({
-            method: "GET",
-            dataType: "json",
-            url: `http://localhost:3000/users?category=${text}`,
+            method: "POST",
+            url: "http://localhost:3000/users",
+            data: {
+                "firstname": firstname,
+                "lastname": lastname,
+                "email": email,
+                "username": username,
+                "password": password,
+                "category": category,
+                "description": briefdes,
+                "date": currentDate
+            },
             success: function (res) {
-                $.each(res.category, function (i, v) {
-                    if (v.category == text) {
-                        alert(v.username);
-                        return;
+                alert('Submitted');
+            },
+            beforeSend: function () {
+                $('.logoForm').fadeOut().fadeIn();
+            }
+        });
+    });
+
+    $("#submitLogForm").submit(function (e) {
+        e.preventDefault();
+        var username = $("#logusername").val();
+        var password = $("#logpassword").val();
+
+        $.ajax({
+            type: "GET",
+            dataType: "json",
+            url: `http://localhost:3000/users?username=${username}&password=${password}`,
+            data: { "username": username, "password": password },
+            success: function (res) {
+                if (res.length == 0) {
+                    $(".result").append('<p class="resultDanger">Incorrect username or password</p>')
+                }
+
+                for (i in res) {
+                    if (res[i].status == 1) {
+                        $(".result").append('<p class="resultDanger">This account has been deleted.</p>')
                     }
-                });
-            }
-        })
-    }
-$("#searchBar").on("keyup", function () {
-    
-});
 
-$('#submitSignForm').submit(function (e) {
-    e.preventDefault();
-
-    var firstname = $('input[name="firstname"]').val();
-    var lastname = $('input[name="lastname"]').val();
-    var email = $('input[name="email"]').val();
-    var username = $('input[name="username"]').val();
-    var password = $('input[name="password"]').val();
-    var category = $('#category').val();
-    var briefdes = $("#briefdes").val();
-    var fullDate = new Date();
-    var twoDigitMonth = ((fullDate.getMonth().length + 1) === 1) ? (fullDate.getMonth() + 1) : '0' + (fullDate.getMonth() + 1);
-    var currentDate = fullDate.getDate() + "/" + twoDigitMonth + "/" + fullDate.getFullYear();
-
-    $.ajax({
-        method: "POST",
-        url: "http://localhost:3000/users",
-        data: {
-            "firstname": firstname,
-            "lastname": lastname,
-            "email": email,
-            "username": username,
-            "password": password,
-            "category": category,
-            "description": briefdes,
-            "date": currentDate
-        },
-        success: function (res) {
-            alert('Submitted');
-        },
-        beforeSend: function () {
-            $('.logoForm').fadeOut().fadeIn();
-        }
-    });
-});
-
-$("#submitLogForm").submit(function (e) {
-    e.preventDefault();
-    var username = $("#logusername").val();
-    var password = $("#logpassword").val();
-
-    $.ajax({
-        type: "GET",
-        dataType: "json",
-        url: `http://localhost:3000/users?username=${username}&password=${password}`,
-        data: { "username": username, "password": password },
-        success: function (res) {
-            if (res.length == 0) {
-                $(".result").append('<p class="resultDanger">Incorrect username or password</p>')
-            }
-
-            for (i in res) {
-                if (res[i].status == 1) {
-                    $(".result").append('<p class="resultDanger">This account has been deleted.</p>')
+                    else {
+                        window.location.assign(`dashboard.html?username=${username}`);
+                    }
                 }
-
-                else {
-                    window.location.assign(`dashboard.html?username=${username}`);
-                }
+            },
+            beforeSend: function () {
+                setInterval(function () {
+                    $(".logoFrame").fadeOut().fadeIn();
+                }, 1000);
             }
-        },
-        beforeSend: function () {
-            setInterval(function () {
-                $(".logoFrame").fadeOut().fadeIn();
-            }, 1000);
-        }
+        });
     });
-});
 });
